1d7d209b9f3722a3768e77d54663497ca9a89e0e
baruch yakubov <baruchyaku@gmail.com>
2022-12-14 12:23:18 +0200

    dfgdg

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -3,8 +3,8 @@ export default {
         <header class="app-header">
             <h1>AppSus</h1>
             <nav>
-                <router-link to="/">Home</router-link> | 
-                <router-link to="/about">About</router-link>
+                <!-- <router-link to="/">Home</router-link> | 
+                <router-link to="/about">About</router-link> -->
             </nav>
         </header>
     `,
